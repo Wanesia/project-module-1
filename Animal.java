ecf31e302a64a5b1c139a0c22bc9015b0bd47227
Wanesia <73067011+Wanesia@users.noreply.github.com>
2021-09-21 15:48:12 +0200

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -16,24 +16,44 @@ public class Animal{
     //Setters and getters
 
     public String getType(){
-        return this.type;public class Animal{
-2
-    String name;
-3
-    int xPosition;
-4
-    int yPosition;
-5
-    Animal(String name, int xPosition, int yPosition){
-6
-        this.name = name;
-7
-        this.xPosition = xPosition;
-8
-        this.yPosition = yPosition;
-9
+        return this.type;
     }
-10
+    public int getXPosition(){
+        return this.xPosition;
+    }
+    public int getYPosition(){
+        return this.yPosition;
+    }
+    public void setXPosition(int xPosition){
+        this.xPosition = xPosition;
+    }
+    public void setYPosition(int yPosition){
+        this.yPosition = yPosition;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    //Print position of object
+    public void tellPosition(){
+        if(this.type=="Rabbit"){
+            System.out.println("I am the nice rabbit, i am now standing on square "+this.xPosition+", "+this.yPosition);
+        }
+        else{
+            System.out.println("I am the evil snake, i am now standing on square "+this.xPosition+", "+this.yPosition);
+        }
+    }
+
+    //Move methods
+    public void moveLeftAndDown(){
+        this.yPosition = this.yPosition-1;
+        this.xPosition = this.xPosition-1;
+    }
+    public void moveRightAndUp(){
+        this.yPosition = this.yPosition+1;
+        this.xPosition = this.xPosition+1;
+    }
+}
+
     public void tellPosition(){
 11
         if(this.name=="Rabbit"){
