c39b6adc8e5efd26d858143ae1bc373e1c2ea543
Wanesia <73067011+Wanesia@users.noreply.github.com>
2021-09-22 08:59:38 +0200

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -6,40 +6,42 @@ public class Zoo{
         Animal rabbit = new Animal("Rabbit",2,2);
         Animal snake = new Animal("Snake",9,9);
 
-        //condition to keep game runnning 
+        //condition to keep game runnning
         boolean gameRunning = true;
-        
+
         System.out.println("Animal horror zoo \n");
 
         snake.tellPosition();
         rabbit.tellPosition();
-        do{
+        while(gameRunning){
+            //delay to make it look like nice not just instant printing
+            try {
+
+                TimeUnit.SECONDS.sleep(1);
+
+            }
+            //this one catches exception so TimeUnit.SECONDS.sleep(1); can work
+            catch (InterruptedException e) {
+            }
             snake.moveLeftAndDown();
             snake.tellPosition();
 
             if (!(rabbit.getYPosition()==snake.getYPosition() && snake.getXPosition()==snake.getXPosition())){
                 rabbit.moveRightAndUp();
                 rabbit.tellPosition();
+                //Math.abs is absolute value from what you put in ()
                 if (Math.abs(snake.getXPosition()-rabbit.getXPosition())==1 && Math.abs(snake.getYPosition()-rabbit.getYPosition())==1){
                     System.out.println("Nooooo Please don't eat me");
                 }
             }
-            else if (Math.abs(snake.getXPosition()-rabbit.getXPosition())==1){
-                System.out.println("Nooooo Please don't eat me");
-            }
+
             else{
                 System.out.println("Ahhhrrrr Im eating you ha ha!");
                 gameRunning=false;
             }
-            try {
 
-                TimeUnit.SECONDS.sleep(1);
-
-            } catch (InterruptedException e) {
-            }
-
-        }while(gameRunning);
-        System.out.println("\nRIP\nGame over rabbit ded");
+        }
+        System.out.println("\nRIP\nGame over rabbit ded :((");
 
     }
 }
